5a40de589a2a85326a60f51ba32378d590902d41
curitiba pr <jessfuhar@gmail.com>
2026-09-11 13:07:58 -0300

    admin: novo kit Novo kit

--- src/data/kits.ts
+++ src/data/kits.ts
@@ -1,3 +1,27 @@
 import type { Kit } from './types';
 
-export const kits: Kit[] = [];
+export const kits: Kit[] = [
+  {
+    "id": "novo-kit",
+    "slug": "novo-kit",
+    "name": "Novo kit",
+    "description": "",
+    "price": 249.9,
+    "active": true,
+    "images": [],
+    "items": [
+      {
+        "productId": "legging-essential",
+        "color": "Marrom"
+      },
+      {
+        "productId": "top-essential",
+        "color": "Marrom"
+      },
+      {
+        "productId": "cropped-essential",
+        "color": "Marrom"
+      }
+    ]
+  }
+];
