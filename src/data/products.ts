39410b5e951307449c0983a545b069cbf76b0969
curitiba pr <jessfuhar@gmail.com>
2026-09-11 18:30:54 -0300

    admin: atualiza produto Top Essential Alça Fina

--- src/data/products.ts
+++ src/data/products.ts
@@ -381,6 +381,24 @@ export const products: Product[] = [
             "alt": ""
           }
         ]
+      },
+      {
+        "color": "Azul Royal",
+        "hex": "#13177c",
+        "images": [
+          {
+            "src": "/images/products/novo-produto/azul-royal-1.png",
+            "alt": ""
+          },
+          {
+            "src": "/images/products/novo-produto/azul-royal-2.png",
+            "alt": ""
+          },
+          {
+            "src": "/images/products/novo-produto/azul-royal-3.png",
+            "alt": ""
+          }
+        ]
       }
     ]
   },
