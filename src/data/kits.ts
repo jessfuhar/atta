bc8ad29386e575e0b5176ae5ca94ad64efffafed
curitiba pr <jessfuhar@gmail.com>
2026-09-12 16:11:11 -0300

    admin: remove kit Novo kit

--- src/data/kits.ts
+++ src/data/kits.ts
@@ -1,27 +1,3 @@
 import type { Kit } from './types';
 
-export const kits: Kit[] = [
-  {
-    "id": "novo-kit",
-    "slug": "novo-kit",
-    "name": "Novo kit",
-    "description": "",
-    "price": 249.9,
-    "active": true,
-    "images": [],
-    "items": [
-      {
-        "productId": "legging-essential",
-        "color": "Marrom"
-      },
-      {
-        "productId": "top-essential",
-        "color": "Marrom"
-      },
-      {
-        "productId": "cropped-essential",
-        "color": "Marrom"
-      }
-    ]
-  }
-];
+export const kits: Kit[] = [];
